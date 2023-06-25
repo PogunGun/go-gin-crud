2745177057814258199cae6111522c67b7b2291a
PogunGun <pavel.pogulailo@gmail.com>
2023-06-25 21:19:00 +0300

    crud books operation

---
+++ src/books/book.service.go
@@ -0,0 +1,101 @@
+package books
+
+import (
+	"github.com/PogunGun/go-gin-crud/src/common/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type AddBookRequestBody struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+}
+
+type UpdateBookRequestBody struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+}
+
+func (h handler) AddBook(ctx *gin.Context) {
+	body := AddBookRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var book models.Book
+
+	book.Title = body.Title
+	book.Author = body.Author
+	book.Description = body.Description
+
+	if result := h.DB.Create(&book); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &book)
+}
+func (h handler) GetBooks(ctx *gin.Context) {
+	var books []models.Book
+
+	if result := h.DB.Find(&books); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &books)
+}
+func (h handler) DeleteBook(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var book models.Book
+
+	if result := h.DB.First(&book, id); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	h.DB.Delete(&book)
+
+	ctx.Status(http.StatusOK)
+}
+func (h handler) GetBook(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var book models.Book
+
+	if result := h.DB.First(&book, id); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &book)
+}
+func (h handler) UpdateBook(ctx *gin.Context) {
+	id := ctx.Param("id")
+	body := UpdateBookRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var book models.Book
+
+	if result := h.DB.First(&book, id); result.Error != nil {
+		ctx.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	book.Title = body.Title
+	book.Author = body.Author
+	book.Description = body.Description
+
+	h.DB.Save(&book)
+
+	ctx.JSON(http.StatusOK, &book)
+}
--- src/main.go
+++ src/main.go
@@ -1,1 +1,30 @@
-package src
+package main
+
+import (
+	"github.com/PogunGun/go-gin-crud/src/books"
+	"github.com/PogunGun/go-gin-crud/src/common/database"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("./src/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	router := gin.Default()
+	dbHandler := db.Init(dbUrl)
+
+	books.RegisterRoutes(router, dbHandler)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+	router.Run(":" + port)
+}
